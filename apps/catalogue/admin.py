f5d09d2aee8da3a6d249bba4c58cc838195dac61
Alexander Gaevsky <sasha@sasha0.ru>
2019-10-05 00:49:35 +0300

    Outline model for storing files belonging to digital products.

--- apps/catalogue/admin.py
+++ apps/catalogue/admin.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+
+from .models import ProductFile
+
+
+admin.site.register(ProductFile)
+
+
 from oscar.apps.catalogue.admin import *  # noqa
--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -1,1 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .storages import DigitalProductStorage
+
+
+class ProductFile(models.Model):
+    product = models.ForeignKey('catalogue.Product', related_name='files', on_delete=models.CASCADE)
+    date_created = models.DateTimeField(_("Date created"), auto_now_add=True, db_index=True)
+    date_updated = models.DateTimeField(_("Date updated"), auto_now=True, db_index=True)
+    file = models.FileField(storage=DigitalProductStorage())
+    checksum = models.CharField(max_length=150)
+    size = models.BigIntegerField()
+    mimetype = models.CharField(max_length=255)
+
+
 from oscar.apps.catalogue.models import *  # noqa isort:skip
