6dd46521a6d98aba8ca33fd64b32f0ff9a4ebb18
Alexander Gaevsky <sasha@sasha0.ru>
2019-10-07 17:31:25 +0300

    Override default clearable file input in the model admin, since file URL is not accessible.

--- apps/catalogue/admin.py
+++ apps/catalogue/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import FileInput
 
 from .models import ProductFile
 
 
-admin.site.register(ProductFile)
+class ProductFileAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.FileField: {'widget': FileInput},
+    }
+
+
+admin.site.register(ProductFile, ProductFileAdmin)
 
 
 from oscar.apps.catalogue.admin import *  # noqa
