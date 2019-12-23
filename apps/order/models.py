6e967d0e78ddc259b945896c6201ab2ae563bd7c
Alexander Gaevsky <sasha@sasha0.ru>
2019-12-23 22:44:11 +0200

    Add property to order model to indicate if order is paid.

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
+
+from oscar.apps.order.abstract_models import AbstractOrder
+
+
+class Order(AbstractOrder):
+    @property
+    def is_paid(self):
+        return self.status == settings.OSCAR_ORDER_PAID_STATUS
+
+
 from oscar.apps.order.models import *  # noqa isort:skip
