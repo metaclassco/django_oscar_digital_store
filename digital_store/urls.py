4a8574fdd944f95061389af8db4e524f508a20a5
Alexander Gaevsky <sasha@sasha0.ru>
2019-12-24 14:33:09 +0200

    Configure media files serving.

--- digital_store/urls.py
+++ digital_store/urls.py
@@ -1,9 +1,11 @@
 from django.apps import apps
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(apps.get_app_config('oscar').urls[0])),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
