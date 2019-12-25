ccfdce91bdaa512ca05dc9a213214eb0ea57b887
Alexander Gaevsky <sasha@sasha0.ru>
2019-12-25 14:23:33 +0200

    Customize partner strategy in order to make digital product unavailable if it does not have any digital assets.

---
+++ apps/partner/strategy.py
@@ -0,0 +1,21 @@
+from django.utils.translation import gettext_lazy as _
+
+from oscar.apps.partner.availability import Unavailable
+from oscar.apps.partner.strategy import Default as CoreDefault
+
+
+class DigitalProductUnavailable(Unavailable):
+    message = _('Digital assets are not available.')
+
+
+class Selector(object):
+    def strategy(self, request=None, user=None, **kwargs):
+        return Default(request)
+
+
+class Default(CoreDefault):
+    def availability_policy(self, product, stockrecord):
+        if product.product_class.is_digital and product.files.count() == 0:
+            return DigitalProductUnavailable()
+
+        return super().availability_policy(product, stockrecord)
